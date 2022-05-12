6298f9a68a933001539eddae41a8397773e8af1c
Ethan <23echin@gmail.com>
2022-05-12 12:38:32 -0700

    CSV file finished

--- src/core/csvFile.java
+++ src/core/csvFile.java
@@ -2,37 +2,34 @@ package core;
 
 import Filters.FindCenter;
 import Interfaces.PixelFilter;
+import core.DImage;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 
 public class csvFile implements PixelFilter {
-
-    private static double[] fake_data = {42.3,56.1,784.0};
-
     public static void main(String[] args) throws IOException {
+        File centers = new File("centers");
         writeDataToFile("centers", FindCenter.data);
-        System.out.println(readFile("centers.txt"));
+        System.out.println(readFile("centers"));
 
     }
+
     public static void writeDataToFile(String filePath, String data) {
         try (FileWriter f = new FileWriter(filePath);
              BufferedWriter b = new BufferedWriter(f);
-             PrintWriter writer = new PrintWriter(b))  {
+             PrintWriter writer = new PrintWriter(b);) {
 
-            writer.println(fake_data);
-
+            writer.println(data);
 
         } catch (IOException error) {
             System.err.println("There was a problem writing to the file: " + filePath);
             error.printStackTrace();
         }
     }
+
     public static String readFile(String fileName) throws IOException {
         return new String(Files.readAllBytes(Paths.get(fileName)));
     }
